5e87e85a11fbf05be2b37bff82ade6a30599ad15
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2022-09-05 18:34:49 +0200

    dram testing

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,8 +1,36 @@
 #![no_main]
+//#![no_std]
+//#[macro_use]
+//extern crate alloc;
+
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 use risc0_zkvm_guest::env;
+use::risc0_zkvm_platform::{memory};
+
+pub const DRAM_SIZE: u64 = 26214401;
+use std::alloc::System;
+//use alloc::vec::Vec;
+
+#[global_allocator]
+static GLOBAL: System = System;
+
+#[derive(Debug)]
+pub struct Dram {
+    pub dram: Vec<u8>,
+    code_size: u64,
+}
+
+impl Dram {
+    /// Create a new memory object with default memory size.
+    pub fn new() -> Self {
+        Self {
+            dram: vec![1; 26214401],
+            code_size: 0,
+        }
+    }
+}
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
@@ -16,7 +44,10 @@ fn test_runner(tests: &[&dyn Fn()]) {
 fn trivial_assertion() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    let _ = Dram::new();
+    let start = memory::HEAP.start();
+    let end = memory::HEAP.end();
+    println!("{:?} and {:?}", start, end);
 }
 #[cfg(test)]
 risc0_zkvm_guest::entry!(test_main);
@@ -33,6 +64,9 @@ pub fn main() {
     if a == 1 || b == 1 {
         panic!("Trivial factors")
     }
+    let _ = Dram::new();
+    let start = memory::HEAP.start();
+    let end = memory::HEAP.end();
     // Compute the product while being careful with integer overflow
     let product = a.checked_mul(b).expect("Integer overflow");
     env::commit(&product);
